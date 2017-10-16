ce3d1d214d7d2e70dbd6ae20858c97ac7cd94bc9
leiyutian <leiyutian@ulaiber.com>
2017-10-16 16:07:17 +0800

    [修改]初始化应用

--- server.py
+++ server.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import tornado.ioloop
+import tornado.web
+
+class TestHandler(tornado.web.RequestHandler):
+    
+    def get(self):
+        self.write("test handler")
+
+
+def make_app():
+    return tornado.web.Application([(r"/", TestHandler)])
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
